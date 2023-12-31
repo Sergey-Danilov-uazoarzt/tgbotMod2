cd96b552c88646acafb3ed575fc7061a2ff605d8
Sergey-Danilov-uazoarzt <uazoarzt@mail.ru>
2023-12-31 19:40:20 +0300

    add StatsService

--- src/main/java/ru/skillfactorydemo/tgbot/controllers/CurrencyController.java
+++ src/main/java/ru/skillfactorydemo/tgbot/controllers/CurrencyController.java
@@ -4,10 +4,13 @@ import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ru.skillfactorydemo.tgbot.dto.ValuteCursOnDate;
 import ru.skillfactorydemo.tgbot.service.CentralRussianBankService;
+import ru.skillfactorydemo.tgbot.service.StatsService;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @RestController
@@ -15,6 +18,7 @@ import java.util.List;
 public class CurrencyController {
 
     private final CentralRussianBankService centralRussianBankService;
+    private final StatsService statsService;
 
     @GetMapping("/getCurrencies")
     @ApiOperation(value = "Получение курса всех валют на текущий день")
@@ -26,5 +30,11 @@ public class CurrencyController {
     @ApiOperation(value = "Получение курса определенно валюты на текущий день")
     public ValuteCursOnDate getCourseForCurrency(@PathVariable String code) throws Exception {
         return centralRussianBankService.getCourseForCurrency(code);
+    }
+
+    @GetMapping("/getStats")
+    @ApiOperation(value = "Получение количества пополнений, которые превышают определённую сумму")
+    public int getStatsAboutIncomesThatGreater(@RequestParam(value = "amount") BigDecimal amount) {
+        return statsService.getCountOfIncomesThatGreater(amount);
     }
 }
